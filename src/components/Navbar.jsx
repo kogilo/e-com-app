418600d76545d837bd3d8383477be21b970c3591
kogilo <getabullaothow@gmail.com>
2024-11-22 09:09:41 -0600

    themes with daisyUI added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,28 @@
 import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
+import NavLinks from './NavLinks';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleTheme } from '../features/user/userSlice';
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+
+  const handleTheme = () => {
+    dispatch(toggleTheme());
+  };
+
+  const numItemsInCart = useSelector((state) => state.cartState.numItemsInCart);
+
   return (
     <nav className='bg-base-200'>
-      <div className='navbar align-element '>
+      <div className='navbar align-element'>
         <div className='navbar-start'>
-          {/* Title */}
+          {/* TITLE */}
           <NavLink
             to='/'
-            className='hidden lg:flex btn btn-primary text-3xl items-center '
+            className='hidden lg:flex btn btn-primary text-3xl items-center'
           >
             E-com
           </NavLink>
@@ -23,21 +35,30 @@ const Navbar = () => {
               tabIndex={0}
               className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52'
             >
-              nav links
+              <NavLinks />
             </ul>
           </div>
         </div>
         <div className='navbar-center hidden lg:flex'>
-          <ul className='menu menu-horizontal '>nav links</ul>
+          <ul className='menu menu-horizontal'>
+            <NavLinks />
+          </ul>
         </div>
         <div className='navbar-end'>
-          {/* THEME ICONS */}
-          {/* CART LINK*/}
-          <NavLink to='cart' className='btn btn-ghost btn-circle btn-md ml-4'>
+          {/* THEME SETUP */}
+          <label className='swap swap-rotate'>
+            <input type='checkbox' onChange={handleTheme} />
+            {/* sun icon*/}
+            <BsSunFill className='swap-on h-4 w-4' />
+            {/* moon icon*/}
+            <BsMoonFill className='swap-off h-4 w-4' />
+          </label>
+          {/* CART LINK */}
+          <NavLink to='/cart' className='btn btn-ghost btn-circle btn-md ml-4'>
             <div className='indicator'>
               <BsCart3 className='h-6 w-6' />
               <span className='badge badge-sm badge-primary indicator-item'>
-                8
+                {numItemsInCart}
               </span>
             </div>
           </NavLink>
