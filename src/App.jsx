5b5d4716bcab43f63eba90eb1bae11477b6a7f99
kogilo <getabullaothow@gmail.com>
2024-11-22 12:06:41 -0600

    Products features added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import {
+  About,
+  Cart,
+  Checkout,
+  Error,
   HomeLayout,
   Landing,
-  Error,
-  Products,
-  SingleProduct,
-  Cart,
-  About,
-  Register,
   Login,
-  Checkout,
   Orders,
+  Products,
+  Register,
+  SingleProduct,
 } from './pages';
 
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { ErrorElement } from './components';
+
+// loaders
+import { loader as landingLoader } from './pages/Landing';
+import { loader as singleProductLoader } from './pages/SingleProduct';
+import { loader as productsLoader } from './pages/Products';
+import { loader as checkoutLoader } from './pages/Checkout';
+import { loader as ordersLoader } from './pages/Orders';
+// actions
+import { action as registerAction } from './pages/Register';
+import { action as loginAction } from './pages/Login';
+import { action as checkoutAction } from './components/CheckoutForm';
+import { store } from './store';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 const router = createBrowserRouter([
   {
@@ -23,27 +46,39 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Landing />,
+        errorElement: <ErrorElement />,
+        loader: landingLoader(queryClient),
       },
       {
         path: 'products',
         element: <Products />,
+        errorElement: <ErrorElement />,
+        loader: productsLoader(queryClient),
       },
       {
         path: 'products/:id',
         element: <SingleProduct />,
+        errorElement: <ErrorElement />,
+        loader: singleProductLoader(queryClient),
       },
       {
         path: 'cart',
         element: <Cart />,
       },
-      { path: 'about', element: <About /> },
+      {
+        path: 'about',
+        element: <About />,
+      },
       {
         path: 'checkout',
         element: <Checkout />,
+        loader: checkoutLoader(store),
+        action: checkoutAction(store, queryClient),
       },
       {
         path: 'orders',
         element: <Orders />,
+        loader: ordersLoader(store, queryClient),
       },
     ],
   },
@@ -51,15 +86,22 @@ const router = createBrowserRouter([
     path: '/login',
     element: <Login />,
     errorElement: <Error />,
+    action: loginAction(store),
   },
   {
     path: '/register',
     element: <Register />,
     errorElement: <Error />,
+    action: registerAction,
   },
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 export default App;
