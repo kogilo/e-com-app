c44a5b7e5fc858bcc110d29698447256c03955dd
kogilo <getabullaothow@gmail.com>
2024-11-22 08:57:04 -0600

    NavLink component added

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as FormInput } from './FormInput';
+import Header from './Header';
+export { default as SubmitBtn } from './SubmitBtn';
+export { default as Header } from './Header';
+export { default as Navbar } from './Navbar';
+
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom';
+import { Header, Navbar } from '../components';
 
 const HomeLayout = () => {
   return (
@@ -6,6 +7,8 @@ const HomeLayout = () => {
       <nav>
         <span className='text-4xl text-primary'>Comfy</span>
       </nav>
+      <section className='align-element py-20'><Outlet />
+      </section>
       <Outlet />
     </>
   );
