88cfcd418b6250e886470efde20ab0a65e1bc873
kogilo <getabullaothow@gmail.com>
2024-11-22 12:45:04 -0600

    Filter input added

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,16 +1,16 @@
-const FormInput = ({ label, name, type, defaultValue }) => {
-    return (
-      <div className='form-control '>
-        <label className='label'>
-          <span className='label-text capitalize'>{label}</span>
-        </label>
-        <input
-          type={type}
-          name={name}
-          defaultValue={defaultValue}
-          className='input input-bordered '
-        />
-      </div>
-    );
-  };
-  export default FormInput;
+const FormInput = ({ label, name, type, defaultValue, size }) => {
+  return (
+    <div className='form-control'>
+      <label htmlFor={name} className='label'>
+        <span className='label-text capitalize'>{label}</span>
+      </label>
+      <input
+        type={type}
+        name={name}
+        defaultValue={defaultValue}
+        className={`input input-bordered ${size}`}
+      />
+    </div>
+  );
+};
+export default FormInput;
