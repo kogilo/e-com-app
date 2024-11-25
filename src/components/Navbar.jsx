5dc296f951e76ddfd454769a0021dd3554fc9f77
kogilo <getabullaothow@gmail.com>
2024-11-25 10:08:52 -0600

    Few edition

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
             to='/'
             className='hidden lg:flex btn btn-primary text-3xl items-center'
           >
-            C
+            E-com
           </NavLink>
           {/* DROPDOWN */}
           <div className='dropdown'>
